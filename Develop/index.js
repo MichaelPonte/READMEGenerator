ff86125999e41f61589ca01484722458798bc0ce
Michael Ponte <michaelponte02@hotmail.com>
2022-12-11 19:13:37 -0500

    Commit 1: questions asked, untested

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,32 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["Please enter a description of your application", "Please enter installation instructions", "Please enter the usage of the application", "Please enter contribution guidlines", "Please enter test instructions"];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.appendFile(`${fileName}.txt`, `${data}\n`, (err) => err ? console.error(err) : console.log(`Commit Logged`));
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+    for (const question of questions) {
+        inquirer.prompt([
+            {
+                type: 'input',
+                message: question,
+                name: 'input',
+            },
+        ])
+        .then((response) => {
+            writeToFile('README', response.question);
+        });
+    }
+    
+}
 
 // Function call to initialize app
 init();
