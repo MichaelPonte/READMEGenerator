a1285cd55a18060984881ac73634ccabd11aaf4b
Michael Ponte <michaelponte02@hotmail.com>
2023-03-14 19:46:06 -0400

    fixed rLB func and completed rLL func

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,6 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if (license) {
+  if (license !== "None") {
     return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`;
   } else {
     return ``;
@@ -10,7 +9,12 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== "None") {
+    return `\n* [License](#license)\n`;
+  }
+  return ``;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
