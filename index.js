90011acb928b5a0aae2feaa4e081e456d8f2f848
Michael Ponte <michaelponte02@hotmail.com>
2023-03-14 19:59:45 -0400

    cleared old code

--- index.js
+++ index.js
@@ -7,29 +7,16 @@ const generateMarkdown = require('./utils/generateMarkdown');
 
 
 // TODO: Create an array of questions for user input
-const questions = ["Please enter a description of your application", "Please enter installation instructions", "Please enter the usage of the application", "Please enter contribution guidlines", "Please enter test instructions"];
+const questions = [];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.appendFile(`${fileName}.txt`, `${data}\n`, (err) => err ? console.error(err) : console.log(`Commit Logged`));
+    return fs.appendFile(path.join(process.cwd(), fileName), data);
 }
 
 // TODO: Create a function to initialize app
 function init() {
 
-    for (const question of questions) {
-        inquirer.prompt([
-            {
-                type: 'input',
-                message: question,
-                name: 'input',
-            },
-        ])
-        .then((response) => {
-            writeToFile('README', response.question);
-        });
-    }
-    
 }
 
 // Function call to initialize app
