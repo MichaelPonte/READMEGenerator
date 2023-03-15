2bb86bae263866873d2182b7be65de88070dc522
Michael Ponte <michaelponte02@hotmail.com>
2023-03-14 20:07:57 -0400

    init complete

--- index.js
+++ index.js
@@ -65,7 +65,13 @@ function writeToFile(fileName, data) {
 
 // TODO: Create a function to initialize app
 function init() {
-
+    inquirer.prompt(questions)
+        .then((inquirerResponses) => {
+            console.log('Generating README...');
+            writeToFile('README.md', generateMarkdown({ ...inquirerResponses }));
+        })
+        .then(() => console.log('Successfully wrote to README.md'))
+        .catch((err) => console.error(err));
 }
 
 // Function call to initialize app
