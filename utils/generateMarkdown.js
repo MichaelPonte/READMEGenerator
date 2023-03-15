04cf84c4466e2cc797192664be96eb5f5e93d8d9
Michael Ponte <michaelponte02@hotmail.com>
2023-03-14 20:17:08 -0400

    fixed typo

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -17,7 +17,7 @@ function renderLicenseLink(license) {
 
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if (lisence !== "None") {
+  if (licence !== "None") {
     return `## License
 
     This project is licensed under the ${license} license.`;
